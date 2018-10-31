d69b3a439ee26811ec2f2e616d0795cd56c176d5
Kory Kilpatrick <koryrkilpatrick@gmail.com>
2018-10-31 15:59:37 -0700

    comment

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.listen(PORT, () => {
 var fileId = '';
 var revisionId = '';
 
+// basic tests
 db.sequelize.sync({ force: true, logging: console.log})
   .then(() => db.createUser())
   .then(user => db.createGist(user.dataValues.userId))
